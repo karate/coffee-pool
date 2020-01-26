7e60f98239ef38ff0e6f5c61189e6b4e1eca415a
karate <tsimpidisd@gmail.com>
2020-01-26 16:05:52 +0200

    Replace time strings with js Date objects

--- app/client/timeline.js
+++ app/client/timeline.js
@@ -48,16 +48,8 @@ class Timeline {
     this.windows.innerHTML = '';
     for (var i = 0; i < windows.length; i++) {
       var windowData = windows[i];
-      // Convert start time to Date object
-      var startDate = new Date();
-      var startDateElements = windowData.start.split(':');
-      startDate.setHours(startDateElements[0], startDateElements[1], 0, 0);
-
-      // Convert end time to Date object
-      var endDate = new Date();
-      var endDateElements = windowData.end.split(':');
-      endDate.setHours(endDateElements[0], endDateElements[1], 0, 0);
-
+      var startDate = new Date(windowData.start);
+      var endDate = new Date(windowData.end);
       // Convert start and end times to percentage
       var startPerc = this.getPercentageOfDay(startDate);
       var endPerc = this.getPercentageOfDay(endDate);
@@ -71,7 +63,9 @@ class Timeline {
       win.style.marginLeft = startPerc + "%";
       win.style.width = "{0}%".replace("{0}", endPerc - startPerc);
       // Add text (time span)
-      win.innerHTML = windowData.start + ' - ' + windowData.end;
+      var start = this.formatTime(startDate);
+      var end = this.formatTime(endDate);
+      win.innerHTML = start + ' - ' + end;
       this.windows.appendChild(win);
     }
   }
@@ -88,33 +82,40 @@ class Timeline {
     }
   }
 
-  // Creates a time window
-  addWindow(startTime = null, endTime = null) {
-    if (startTime == null) startTime = this.startTimeElement.value;
-    if (endTime == null) endTime = this.endTimeElement.value;
-    // TESTING: If start and end time is empty, create random window
-    this.myWindows.push({start: startTime, end: endTime});
-    // Clear input fields
-    this.startTimeElement.value = '';
-    this.endTimeElement.value = '';
-
-    this.drawWindows();
-  }
-
-  createWindow(startTime, endTime) {
-    if (!startTime || !endTime) {
-      // Random start/end times
-      var sh = Math.round(this.minHour + (Math.random() * (this.hourSpan - 1)));
-      var eh = Math.round(sh + 1 + (Math.random() * (this.maxHour - sh - 1)));
-
-      var sm = Math.round(Math.random() * 5) * 10;
-      var em = '00';
-
-      startTime = sh + ":" + sm;
-      endTime = eh + ":" + em;
+  createWindow(startDate, endDate) {
+    if (!startDate) {
+      this.startTimeElement = document.getElementById('start-time');
+      if (this.startTimeElement.value) {
+        startDate = new Date();
+        var s = this.startTimeElement.value.split(':');
+        startDate.setHours(s[0]);
+        startDate.setMinutes(s[1]);
+      }
+      else {
+        // Random start/end times
+        var startDate = new Date();
+        startDate.setHours(Math.round(this.minHour + (Math.random() * (this.hourSpan - 1))));
+        startDate.setMinutes(Math.round(Math.random() * 5) * 10);
+      }
+    }
+    if (!endDate) {
+      this.endTimeElement = document.getElementById('end-time');
+      if (this.endTimeElement.value) {
+        endDate = new Date();
+        var s = this.endTimeElement.value.split(':');
+        endDate.setHours(s[0]);
+        endDate.setMinutes(s[1]);
+      }
+      else {
+        // Random start/end times
+        var endDate = new Date();
+        var startDateHours = startDate.getHours();
+        endDate.setHours(Math.round(startDateHours + 1 + (Math.random() * (this.maxHour - startDateHours - 1))));
+        endDate.setMinutes(0);
+      }
     }
     //this.addWindow(startTime, endTime);
-    window.ws.sendWindow({start: startTime, end: endTime})
+    window.ws.sendWindow({start: startDate, end: endDate})
   }
 
   populateStartEndTimeInputs() {
